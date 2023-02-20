d4ae84576cf78d60dd1380d316b576db47f2de52
Sebastien Druon <seb@homedruon.com>
2023-02-20 11:08:50 +0100

    Grabbing a single frame

--- cam_client/src/main.c
+++ cam_client/src/main.c
@@ -1,10 +1,52 @@
+/* SPDX-License-Identifier:Unlicense */
+
+/* Aravis header */
+
+#include <arv.h>
+
+/* Standard headers */
+
+#include <stdlib.h>
 #include <stdio.h>
 
-int main( int argc, char** argv, char** envv ) {
+/*
+ * Connect to the first available camera, then acquire a single buffer.
+ */
 
-    printf("Hello world\n");
+int
+main (int argc, char **argv)
+{
+	ArvCamera *camera;
+	ArvBuffer *buffer;
+	GError *error = NULL;
 
-    return 0 ;
+	/* Connect to the first available camera */
+	camera = arv_camera_new (NULL, &error);
+
+	if (ARV_IS_CAMERA (camera)) {
+		printf ("Found camera '%s'\n", arv_camera_get_model_name (camera, NULL));
+
+		/* Acquire a single buffer */
+		buffer = arv_camera_acquisition (camera, 0, &error);
+
+		if (ARV_IS_BUFFER (buffer)) {
+			/* Display some informations about the retrieved buffer */
+			printf ("Acquired %d×%d buffer\n",
+				arv_buffer_get_image_width (buffer),
+				arv_buffer_get_image_height (buffer));
+			/* Destroy the buffer */
+			g_clear_object (&buffer);
+		}
+
+		/* Destroy the camera instance */
+		g_clear_object (&camera);
+	}
+
+	if (error != NULL) {
+		/* En error happened, display the correspdonding message */
+		printf ("Error: %s\n", error->message);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
-
-
