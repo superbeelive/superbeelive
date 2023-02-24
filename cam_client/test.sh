e7632c754d8ec85f5ee7cf926457ee6127b98d26
Sebastien Druon <seb@homedruon.com>
2023-02-24 13:31:30 +0100

    compression with FFV1 version 3 is good !

--- cam_client/test.sh
+++ cam_client/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ffmpeg -f rawvideo -vcodec rawvideo -pix_fmt gray -s 1920x1080 -r 30 -i $1 \
-	-c:v libx264 -pix_fmt gray -crf 0 -preset:v slow -c:a copy output.mkv
+	-c:v ffv1 -level 3 output.mkv
 
 ffmpeg -i output.mkv -vcodec rawvideo -pix_fmt gray -f rawvideo output.raw
