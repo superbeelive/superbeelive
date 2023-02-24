79ee2302feff238537ea3f5f06bdf2458124da83
Sebastien Druon <seb@homedruon.com>
2023-02-24 17:02:25 +0100

    ???

--- sbl_video_compress/test.sh
+++ sbl_video_compress/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ffmpeg -f rawvideo -vcodec rawvideo -pix_fmt gray -s 1920x1080 -r 30 -i $1 \
+ffmpeg -f rawvideo -vcodec rawvideo -pix_fmt gray -s 1920x1200 -r 30 -i $1 \
 	-c:v ffv1 -level 3 output.mkv
 
 ffmpeg -i output.mkv -vcodec rawvideo -pix_fmt gray -f rawvideo output.raw
